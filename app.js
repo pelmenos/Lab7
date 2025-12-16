98dc3bebb9c8a01e5ba504df31e6176af7a59b93
pelmenos <vadimshvynda@gmail.com>
2025-12-16 13:27:56 +0700

    first

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+    res.status(200);
+    res.send('Hello world!')
+})
+
+app.listen(PORT, () => console.log(`Start app ${PORT}`))
